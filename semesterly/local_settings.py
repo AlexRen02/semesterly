b057fc5c8362c0d7baa0b7ff0b4a38353149618f
Alex Ren <42788968+AlexRen02@users.noreply.github.com>
2020-07-16 13:32:49 -0700

    Update local_settings.py

--- semesterly/local_settings.py
+++ semesterly/local_settings.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'aren3',
         'PASSWORD': 'Swimguy1',
         'HOST': 'db',
         'PORT': '5432',
