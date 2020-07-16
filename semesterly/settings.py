3361dc848a65d23fe580090e7682bbadb21340b7
Alex Ren <42788968+AlexRen02@users.noreply.github.com>
2020-07-16 14:53:51 -0700

    Update settings.py

--- semesterly/settings.py
+++ semesterly/settings.py
@@ -234,8 +234,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '',  # os.path.join(BASE_DIR, 'db.postgresql')
-        'USER': 'aren3',
-        'PASSWORD': '12345',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
